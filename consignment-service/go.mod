50f00aaf217598314de64a761bf78a743484ac4d
ryanyogan <ryanyogan@gmail.com>
2019-12-16 16:19:30 -0800

    :ad

--- consignment-service/go.mod
+++ consignment-service/go.mod
@@ -1,13 +1,14 @@
-module github.com/ryanyogan/transport-consignment
+module github.com/ryanyogan/transport/consignment-service
 
 go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.18.0
+	github.com/micro/protobuf v0.0.0-20180321161605-ebd3be6d4fdb // indirect
 	github.com/ryanyogan/transport v0.0.0-20191215015922-6075d6bec9e1
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
 	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20191206224255-0243a4be9c8f // indirect
 	google.golang.org/grpc v1.25.1
 )
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"net"
-	"sync"
-
 	"context"
-	"log"
+	"fmt"
 
+	"github.com/micro/go-micro"
 	pb "github.com/ryanyogan/transport/consignment-service/proto/consignment"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-)
-
-const (
-	port = ":50051"
 )
 
 type repository interface {
@@ -24,16 +16,13 @@ type repository interface {
 // Repository - Dummy repo, this simulates the use of a datastore
 // of some kind. We'll replace with real later
 type Repository struct {
-	mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 // Create a new consignment
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
-	repo.mu.Unlock()
 	return consignment, nil
 }
 
@@ -53,37 +42,34 @@ type service struct {
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 	consignment, err := s.repo.Create(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &pb.Response{Created: true, Consignment: consignment}, nil
+	res.Created = true
+	res.Consignment = consignment
+	return nil
 }
 
 // GetConsignments - will return all consignments from the active repository
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	consignments := s.repo.GetAll()
-	return &pb.Response{Consignments: consignments}, nil
+	res.Consignments = consignments
+	return nil
 }
 
 func main() {
 	repo := &Repository{}
 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+	srv := micro.NewService(
+		micro.Name("transport.service.consignment"),
+	)
 
-	s := grpc.NewServer()
+	srv.Init()
 
-	pb.RegisterShippingServiceServer(s, &service{repo})
-
-	reflection.Register(s)
-
-	log.Println("Runnin on port:", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
 	}
 }
